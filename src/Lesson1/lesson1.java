b094ddb439d6ee0715cb61541092dd1a9aa5b3c6
PODmitry <dm-shut84@mail.ru>
2022-05-22 20:39:57 +0500

    Homework_Lesson1_Popov.D.O

---
+++ src/Lesson1/lesson1.java
@@ -0,0 +1,49 @@
+package Lesson1;
+
+public class lesson1  {
+
+  public static void main(String[] args){
+    printThreeWords();
+    checkSumSign();
+    printColor();
+    compareNumbers();
+  }
+
+  public static void printThreeWords(){
+    System.out.println("\nOrange\nBanana\nApple");
+  }
+
+  public static void checkSumSign(){
+    int a = -3;
+    int b = 2;
+    int c = a + b;
+    if ( c >= 0 )
+      System.out.println("Сумма положительная");
+    else
+      System.out.println("Сумма отрицательная");
+
+  }
+
+  public static void printColor(){
+
+    int value = 101;
+
+    if (value <=0)
+      System.out.println("Красный");
+    if (value > 0 && value <= 100)
+      System.out.println("Жёлтый");
+    if (value > 100)
+      System.out.println("Зелёный");
+  }
+
+  public static void compareNumbers(){
+
+    int a = 1;
+    int b = 2;
+
+    if (a >= b)
+      System.out.println("a >= b");
+    else
+      System.out.println("a < b");
+  }
+}
